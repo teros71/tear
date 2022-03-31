d515f9fdb9b2bb84e8184243cd8410261b33fbcc
Tero Suomela <tero.suomela@iki.fi>
2022-03-31 11:29:05 +0200

    refactoring

--- tear/svg/writer.py
+++ tear/svg/writer.py
@@ -7,101 +7,138 @@ from tear.model import store
 
 log = logging.getLogger(__name__)
 
-
-def write_svg_feshadow(file, id):
-    file.write(f'<filter id="{id}">'
-               '<feGaussianBlur in="SourceAlpha" stdDeviation="3" />'
-               '<feOffset dx="2" dy="4" />'
-               '<feMerge>'
-               '<feMergeNode />'
-               '<feMergeNode in="SourceGraphic" />'
-               '</feMerge>'
-               '</filter>')
-
-
-def write_svg_feblur(file, id):
-    file.write(f'<filter id="{id}">'
-               '<feGaussianBlur in="SourceGraphic" stdDeviation="5" />'
-               '</filter>')
-
-
-def write_svg_clip(file, id, shap):
-    file.write(f'<clipPath id="{id}">')
-
-    def write_one_clip(s):
-        if isinstance(s, shape.List):
-            for ss in s:
-                write_one_clip(ss)
-            return
-        if isinstance(s.g, geom.Rect):
-            write_svg_rect(file, s.g)
-        elif isinstance(s.g, geom.Circle):
-            write_svg_circle(file, s.g)
-        elif isinstance(s.g, geom.Ellipse):
-            write_svg_ellipse(file, s)
-        else:
-            write_svg_polygon(file, s.g)
-        file.write(' />\n')
-    write_one_clip(shap)
-    file.write('</clipPath>')
-#      <defs>
-#      </defs>
-
-
-def write_svg_mask(file, id, shap):
-    """write image"""
-    file.write(f'<mask id="{id}">')
-    file.write(
-        f'<rect x="0" y="0" width="100%" height="100%" fill="white" />\n')
-    write_svg_geom(file, shap)
-    file.write('fill="black" />')
-    file.write('</mask>\n')
-    return
-
-
-def write_svg_recursive(file, shapes):
-    """write recursive form"""
-    if not isinstance(shapes, list):
-        #print(shapes)
-        single = shapes
-#        if not isinstance(shapes, shape.Shape):
-#            shap = shape.Shape(polygon.Polygon(shapes.get_points()))
-        write_svg_shape(file, single)
-        return
-    log.debug("recursive list shape")
-    for shap in shapes:
-        write_svg_recursive(file, shap)
-
-
 SHAPE_COUNT = 0
 
 
-def write_svg_rect(file, g):
+def write(fname, height, width, wbh, wbw, config):
+    """Write svg file"""
+    bg = config.get("background", "white")
+    shapes = config.get("shapes", [])
+#    images = config.get("images", [])
+    with open(fname, 'w', encoding="utf-8") as file:
+        file.write(
+            f'<svg style="background-color:{bg}" '
+            f'viewBox="0 0 {wbw} {wbh}" '
+            f'height="{height}" width="{width}" '
+            f'xmlns="http://www.w3.org/2000/svg">\n')
+        write_clips(file)
+        for ss in shapes:
+            sn, s = get_shape(ss)
+            if s is None:
+                log.warning("shape is not available;name=%s",
+                            sn if sn else "-")
+            else:
+                log.info("writing shape;name=%s", sn)
+                write_shape(file, s)
+#        for img in images:
+#            write_image(file, img, bg)
+        file.write('</svg>\n')
+
+
+def get_shape(config):
+    """write a form"""
+    if isinstance(config, dict):
+        # old style dict with name
+        name = config.get('name')
+        if name is None:
+            log.warning("shape name not available")
+            return None, None
+    elif isinstance(config, str):
+        name = config
+    else:
+        log.error("unsupported config")
+        return None, None
+    return name, store.get_shape(name)
+
+
+def write_shape(file, shap):
+    """write shape"""
+    global SHAPE_COUNT
+
+    if isinstance(shap, shape.List):
+        write_list(file, shap)
+        return
+    SHAPE_COUNT += 1
+    app = shap.appearance
+    if app.shadow:
+        write_feshadow(file, f'shadow_n{SHAPE_COUNT}')
+    if app.blur:
+        write_feblur(file, f'blur_n{SHAPE_COUNT}')
+    write_geom(file, shap)
+    write_style(file, app)
+    if app.shadow:
+        file.write(f' filter="url(#shadow_n{SHAPE_COUNT})"')
+    if app.blur:
+        file.write(f' filter="url(#blur_n{SHAPE_COUNT})"')
+    if app.mask is not None:
+        file.write(f' mask="url(#{app.mask})"')
+    file.write(' />\n')
+    #if isinstance(single.base, path.Path):
+    #    write_svg_path_debug(file, single.base)
+
+
+def write_list(file, lst):
+    global SHAPE_COUNT
+    SHAPE_COUNT += 1
+
+    if lst.appearance.blur:
+        write_feblur(file, f'blur_n{SHAPE_COUNT}')
+
+    file.write('<g')
+    if lst.appearance.clip is not None:
+        file.write(f' clip-path="url(#{lst.appearance.clip})"')
+    if lst.appearance.blur:
+        file.write(f' filter="url(#blur_n{SHAPE_COUNT})"')
+    file.write('>')
+    for s in lst:
+        write_shape(file, s)
+    file.write('</g>')
+
+
+# write geometry objects
+
+def write_geom(file, shap):
+    """Write geometry of shape"""
+    if isinstance(shap.g, geom.Rect):
+        write_rect(file, shap.g)
+    elif isinstance(shap.g, geom.Circle):
+        write_circle(file, shap.g)
+    elif isinstance(shap.g, geom.Ellipse):
+        write_ellipse(file, shap.g, shap.angle)
+    elif isinstance(shap.g, geom.Polygon):
+        write_polygon(file, shap.g)
+    elif isinstance(shap.g, path.Path):
+        write_path(file, shap.g)
+
+
+def write_rect(file, g):
+    """rect"""
     x = g.position.x - g.width / 2
     y = g.position.y - g.height / 2
     file.write(f'<rect x="{x}" y="{y}" '
                f'height="{g.height}" width="{g.width}" ')
 
 
-def write_svg_circle(file, g):
+def write_circle(file, g):
+    """circle"""
     x = g.position.x
     y = g.position.y
     file.write(f'<circle cx="{x}" cy="{y}" r="{g.radius}" ')
 
 
-def write_svg_ellipse(file, e):
-    g = e.g
+def write_ellipse(file, g, angle):
+    """ellipse"""
     x = g.position.x
     y = g.position.y
     rx = g.rx
     ry = g.ry
     file.write(f'<ellipse cx="{x}" cy="{y}" rx="{rx}" ry="{ry}" ')
-    if e.angle != 0:
-        a = e.angle
-        file.write(f'transform="rotate({a} {x} {y})" ')
+    if angle != 0:
+        file.write(f'transform="rotate({angle} {x} {y})" ')
 
 
-def write_svg_polygon(file, poly):
+def write_polygon(file, poly):
+    """polygon"""
     file.write('<polygon\n')
     file.write('points="')
     for p in poly.get_points():
@@ -109,7 +146,7 @@ def write_svg_polygon(file, poly):
     file.write('"\n')
 
 
-def write_svg_path(file, g):
+def write_path(file, g):
     """Write path"""
 
     def write_cubic(segs):
@@ -143,6 +180,7 @@ def write_svg_path(file, g):
 
 
 def write_svg_path_debug(file, path):
+    """debug path"""
     def wp(p, c):
         file.write(
             f'<circle cx="{p.x}" cy="{p.y}" r="10" fill="{c}" opacity="0.5"/>')
@@ -158,55 +196,65 @@ def write_svg_path_debug(file, path):
             wp(s.cp, "green")
 
 
-def write_svg_style(file, app):
+def write_style(file, app):
+    """Style settings for object"""
     file.write(
         f'style="opacity:{app.opacity};'
         f'fill:{app.colour};stroke:{app.stroke};'
         f'stroke-width:{app.stroke_width};stroke-linejoin:round"')
 
-
-def write_svg_geom(file, shap):
-    if isinstance(shap.g, geom.Rect):
-        write_svg_rect(file, shap.g)
-    elif isinstance(shap.g, geom.Circle):
-        write_svg_circle(file, shap.g)
-    elif isinstance(shap.g, geom.Ellipse):
-        write_svg_ellipse(file, shap)
-    elif isinstance(shap.g, geom.Polygon):
-        write_svg_polygon(file, shap.g)
-    elif isinstance(shap.g, path.Path):
-        write_svg_path(file, shap.g)
+# filters
 
 
-def write_svg_shape(file, single):
-    """write shape"""
-    global SHAPE_COUNT
-    SHAPE_COUNT += 1
-    if isinstance(single, shape.List):
-        if single.appearance.clip is not None:
-            file.write(f'<g clip-path="url(#{single.appearance.clip})">')
+def write_feshadow(file, id):
+    file.write(f'<filter id="{id}">'
+               '<feGaussianBlur in="SourceAlpha" stdDeviation="3" />'
+               '<feOffset dx="2" dy="4" />'
+               '<feMerge>'
+               '<feMergeNode />'
+               '<feMergeNode in="SourceGraphic" />'
+               '</feMerge>'
+               '</filter>')
+
+
+def write_feblur(file, id):
+    file.write(f'<filter id="{id}">'
+               '<feGaussianBlur in="SourceGraphic" stdDeviation="5" />'
+               '</filter>')
+
+
+def write_svg_clip(file, id, shap):
+    file.write(f'<clipPath id="{id}">')
+
+    def write_one_clip(s):
+        if isinstance(s, shape.List):
+            for ss in s:
+                write_one_clip(ss)
+            return
+        if isinstance(s.g, geom.Rect):
+            write_rect(file, s.g)
+        elif isinstance(s.g, geom.Circle):
+            write_circle(file, s.g)
+        elif isinstance(s.g, geom.Ellipse):
+            write_ellipse(file, s.g, s.angle)
         else:
-            file.write('<g>')
-        for b in single.shapes:
-            write_svg_recursive(file, b)
-        file.write('</g>')
-        return
-    app = single.appearance
-    if app.shadow:
-        write_svg_feshadow(file, f'shadow_n{SHAPE_COUNT}')
-    if app.blur:
-        write_svg_feblur(file, f'blur_n{SHAPE_COUNT}')
-    write_svg_geom(file, single)
-    write_svg_style(file, app)
-    if app.shadow:
-        file.write(f' filter="url(#shadow_n{SHAPE_COUNT})"')
-    if app.blur:
-        file.write(f' filter="url(#blur_n{SHAPE_COUNT})"')
-    if app.mask is not None:
-        file.write(f' mask="url(#{app.mask})"')
-    file.write(' />\n')
-    #if isinstance(single.base, path.Path):
-    #    write_svg_path_debug(file, single.base)
+            write_polygon(file, s.g)
+        file.write(' />\n')
+    write_one_clip(shap)
+    file.write('</clipPath>')
+#      <defs>
+#      </defs>
+
+
+def write_svg_mask(file, id, shap):
+    """write image"""
+    file.write(f'<mask id="{id}">')
+    file.write(
+        f'<rect x="0" y="0" width="100%" height="100%" fill="white" />\n')
+    write_geom(file, shap)
+    file.write('fill="black" />')
+    file.write('</mask>\n')
+    return
 
 
 def write_svg_image(file, img, w, h, bg):
@@ -220,23 +268,6 @@ def write_svg_image(file, img, w, h, bg):
                f'height="{h}" width="{w}" '
                f'fill="{bg}" mask="url(#{img.name})" />\n')
     return
-
-
-def write_form(file, config, w, h, bg):
-    """write a form"""
-    if isinstance(config, dict):
-        name = config.get('name')
-        if name is None:
-            return
-    elif isinstance(config, str):
-        name = config
-    log.info(f"getting shape {name}")
-    sss = store.get_shape(name)
-    if sss is not None:
-        #        print(sss)
-        write_svg_recursive(file, sss)
-#    else:
-#        write_image(file, name, w, h, bg)
 
 
 #def write_image(file, name, w, h, bg):
@@ -257,21 +288,3 @@ def write_clips(file):
         mask = store.get_shape(name)
         write_svg_mask(file, maskid, mask)
     file.write('</defs>')
-
-
-def write(fname, height, width, wbh, wbw, config):
-    bg = config.get("background", "white")
-    shapes = config.get("shapes", [])
-    images = config.get("images", [])
-    with open(fname, 'w', encoding="utf-8") as file:
-        file.write(
-            f'<svg style="background-color:{bg}" '
-            f'viewBox="0 0 {wbw} {wbh}" '
-            f'height="{height}" width="{width}" '
-            f'xmlns="http://www.w3.org/2000/svg">\n')
-        write_clips(file)
-        for fn in shapes:
-            write_form(file, fn, width, height, bg)
-#        for img in images:
-#            write_image(file, img, bg)
-        file.write('</svg>\n')
